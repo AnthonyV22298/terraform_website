832befabd6482356ecf9e0dc58b5f009858f9eab
AnthonyV22298 <47114515+AnthonyV22298@users.noreply.github.com>
2020-10-21 10:54:46 -0400

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,40 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "VargasNetwork"
+        workspaces {
+            prefix = "terraform_website"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "aws_static_site" {
+    source  = "AnthonyV22298/terraform_website/aws"
+    version = "1.2.0"
+
+    domain = "static-site-demo.davidvargas.me"
+    secret = "secret-key"
+    tags = {
+        Application = "terraform_website"
+    }
+}
+
+provider "github" {
+    owner = "AnthonyV22298"
+}
+
+resource "github_actions_secret" "deploy_aws_access_key" {
+  repository       = "static-site-demo"
+  secret_name      = "DEPLOY_AWS_ACCESS_KEY_ID"
+  plaintext_value  = module.aws_static_site.deploy-id     
+}
+
+resource "github_actions_secret" "deploy_aws_access_secret" {
+  repository       = "static-site-demo"
+  secret_name      = "DEPLOY_AWS_SECRET_ACCESS_KEY"       
+  plaintext_value  = module.aws_static_site.deploy-secret 
+}
