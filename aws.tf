a71472c7557efcb9f88a25184659427b27c6b46e
AnthonyV22298 <47114515+AnthonyV22298@users.noreply.github.com>
2020-10-21 10:58:22 -0400

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -16,7 +16,7 @@ module "aws_static_site" {
     source  = "AnthonyV22298/terraform_website/aws"
     version = "1.2.0"
 
-    domain = "static-site-demo.davidvargas.me"
+    domain = "duckfarts.net"
     secret = "secret-key"
     tags = {
         Application = "terraform_website"
